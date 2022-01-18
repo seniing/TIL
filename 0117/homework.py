81339209ae70f82c56d0cd760c07cb473b3d2ae1
seniing <zmmmm111@gmail.com>
2022-01-18 09:51:36 +0900

    220117 TIL

---
+++ 0117/homework.py
@@ -0,0 +1,65 @@
+#1
+# import keyword
+# print(keyword.kwlist)
+
+
+
+# 2
+# num1 = 0.1 * 3
+# num2 = 0.3
+
+# import math
+# print(math.isclose(num1, num2))
+
+# print(abs(num1 - num2) <= 1e-10)
+
+
+
+# 4
+# name = '철수'
+# print(f'안녕, {name}야')
+# print('안녕, {}야'.format(name))
+# # print('안녕, %s야' %name)
+
+
+
+# 5
+# str(1) #(1)
+# int('30') #(2)
+# int(5) #(3)
+# bool('50') #(4)
+
+
+# print(str(1))
+# print(int('30'))
+# print(int(5))
+# print(bool('50'))
+
+
+
+# 6
+# n = 5
+# m = 9
+
+# x = '*' * n 
+# print((('*' * n ) + '\n') * m)
+# print((x + '\n') * m)
+
+
+
+# 7
+# print('"파일은 c:\Windows\\User\내문서\Python에 저장이 되었습니다." \n 나는 생각했다. \'cd를 써서 git bash로 들어가 봐야지.\'')
+#print('''"파일은 c:\Windows\\User\내문서\Python에 저장이 되었습니다."
+#나는 생각했다. \'cd를 써서 git bash로 들어가 봐야지.\'''')
+
+
+
+# 8
+# a = 1
+# b = 2
+# c = 1
+
+# x1 = (-b + (b**2 - 4 * a *c)) / (2 * a)
+# x2 = (-b - (b**2 - 4 * a *c)) / (2 * a)
+
+# print(x1, x2)
---
+++ 0117/workshop.py
@@ -0,0 +1,29 @@
+#1
+# number = int(input())
+
+# for i in range(1, number+1):
+#     print(i)
+
+
+
+#2
+# number = int(input())
+
+# for i in range(number, -1, -1):
+#     print(i)
+
+
+
+#3
+number = int(input())
+num = 0
+total = 0
+
+# for i in range(1, number+1):
+#     total += i
+# print(total)
+
+while b <= number:
+    total += num
+    num += 1
+print(total)
